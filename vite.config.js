b4fe0ca49fa7d8264e4b8c28e8d860344525967d
Olamide Ismail <abolarinwaismail332@gmail.com>
2026-05-01 07:37:58 +0100

    remove node_modules from git

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+
+  // ✅ THIS FIXES YOUR BLANK SITE
+  base: '/aphth-tech-hub/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
